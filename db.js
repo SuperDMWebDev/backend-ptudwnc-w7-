9704ea01b35c2127009747048a9381b166f07ca1
minh <hadtnt71@gmail.com>
2022-10-30 14:26:46 +0700

    change package.json

--- db.js
+++ db.js
@@ -11,7 +11,7 @@ const db = mysql.createConnection({
 
 db.connect(function (err) {
   if (err) throw err;
-  console.log("Connected");
+  console.log("Connected", process.env.DB_HOST);
 });
 
 module.exports = db;
